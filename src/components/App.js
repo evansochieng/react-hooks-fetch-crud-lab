88b2baed9e9736cd1017c80ff538ed8e243eb79b
Evans Ochieng <evansochieng121@gmail.com>
2022-09-27 11:02:56 +0300

    Handled GET /questions and displayed on the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  // Fetch data from the API, set state to the results
+  useEffect( () => {
+    fetch('http://localhost:4000/questions')
+    .then(response => response.json())
+    .then(data => setQuestions(data))
+  }, [])
+
+  // 
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList questions={questions}/>}
     </main>
   );
 }
